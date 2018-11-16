5959b8ae38f0d7751514b4b46b95bc69fb0454c3
Matheus Vieira <130126861@aluno.unb.br>
2018-11-16 18:40:47 -0200

    Pre processamento do arquivo feito

--- Trabalho 2 de SB/Montador.cpp
+++ Trabalho 2 de SB/Montador.cpp
@@ -1,12 +1,111 @@
 #include "pch.h"
 #include "Montador.h"
 
+#include <iostream>
 
-Montador::Montador()
-{
+Montador::Montador(){
 }
 
 
-Montador::~Montador()
+Montador::~Montador(){
+}
+
+Montador::TokensDaLinha::TokensDaLinha(const std::string & label, const std::string & operacao, const std::vector<std::string>& operando, int numeroDaLinha){
+}
+
+void Montador::adicionarTokenDaLinha(TokensDaLinha linha){
+	Montador::listaDeTokensDoArquivo.push_back(linha);
+}
+
+void Montador::PreProcessamento(std::string conteudoArquivo) {
+	std::vector<TokensDaLinha> tokensDaLinha;
+
+	conteudoArquivo = RemoveComentarios(conteudoArquivo);
+	conteudoArquivo = RemoveTabulacoes(conteudoArquivo);
+	conteudoArquivo = RemoveEspacosEmBrancoExtras(conteudoArquivo);
+	conteudoArquivo = JuntaLabelEOperacao(conteudoArquivo);
+
+}
+
+std::string Montador::RemoveComentarios(std::string arquivoConteudo) {
+	std::string::size_type posicaoComentario = posicaoComentario = arquivoConteudo.find(';');
+	while (posicaoComentario != std::string::npos) {
+
+		std::string::size_type posicaoFinal = posicaoComentario;
+		while (arquivoConteudo[posicaoFinal] != '\n') {
+			posicaoFinal++;
+		}
+
+		arquivoConteudo.erase(arquivoConteudo.begin() + posicaoComentario, arquivoConteudo.begin() + posicaoFinal);
+		posicaoComentario = arquivoConteudo.find(';');
+	}
+	return arquivoConteudo;
+}
+
+std::string Montador::RemoveTabulacoes(std::string fileString) {
+	// Remove tabulações transformando-as em espaços em branco, caso os espaços em branco sejam redundantes, 
+	// o passo de remover espaços em branco irá remover a redundância.
+	fileString = std::regex_replace(fileString, std::regex("\t"), " ");
+	// fileString.erase(std::remove(fileString.begin(), fileString.end(), '\t'), fileString.end());
+	return fileString;
+}
+
+std::string Montador::RemoveEspacosEmBrancoExtras(std::string arquivoConteudo) {
+
+	//remove duplos espaços
+	std::string::size_type posicaoEspaco = arquivoConteudo.find("  ");
+	while (posicaoEspaco != std::string::npos) {
+		arquivoConteudo.erase(arquivoConteudo.begin() + posicaoEspaco);
+		posicaoEspaco = arquivoConteudo.find("  ");
+	}
+
+	//remove duplos pulos de linha
+	posicaoEspaco = arquivoConteudo.find("\n\n");
+	while (posicaoEspaco != std::string::npos) {
+		arquivoConteudo.erase(arquivoConteudo.begin() + posicaoEspaco);
+		posicaoEspaco = arquivoConteudo.find("\n\n");
+	}
+
+	////remove duplos pulos no final do arquivo
+	if (arquivoConteudo[arquivoConteudo.size() - 1] == '\n') {
+		arquivoConteudo.erase(arquivoConteudo.end()-1);
+	}
+
+	//remove duplos pulos no inicio do arquivo
+	if (arquivoConteudo[0] == '\n') {
+		arquivoConteudo.erase(arquivoConteudo.begin());
+	}
+
+	//remove espaços antes de \n
+	posicaoEspaco = arquivoConteudo.find(" \n");
+	while (posicaoEspaco != std::string::npos) {
+		arquivoConteudo.erase(arquivoConteudo.begin() + posicaoEspaco);
+		posicaoEspaco = arquivoConteudo.find(" \n");
+	}
+
+	//remove espaços depois de \n
+	posicaoEspaco = arquivoConteudo.find("\n ");
+	while (posicaoEspaco != std::string::npos) {
+		arquivoConteudo.erase(arquivoConteudo.begin() + posicaoEspaco + 1);
+		posicaoEspaco = arquivoConteudo.find("\n ");
+	}
+
+	//remove espaços antes de :
+	posicaoEspaco = arquivoConteudo.find(" :");
+	while (posicaoEspaco != std::string::npos) {
+		arquivoConteudo.erase(arquivoConteudo.begin() + posicaoEspaco);
+		posicaoEspaco = arquivoConteudo.find(" :");
+	}
+
+	return arquivoConteudo;
+}
+
+std::string Montador::JuntaLabelEOperacao(std::string arquivo)
 {
+	std::string::size_type posicao = arquivo.find(":\n");
+	while (posicao != std::string::npos) {
+		arquivo[posicao + 1] = ' ';
+		posicao = arquivo.find(":\n");
+	}
+	return arquivo;
 }
--- Trabalho 2 de SB/Montador.h
+++ Trabalho 2 de SB/Montador.h
@@ -1,6 +1,33 @@
 #pragma once
+
+#include <string>
+#include <vector>
+#include <regex>
+
 class Montador {
 public:
+	struct TokensDaLinha {
+		std::string label;
+		std::string operacao;
+		std::vector<std::string> operando;
+
+		TokensDaLinha(const std::string &label, const std::string &operacao, const std::vector<std::string> &operando,
+			int numeroDaLinha);
+	};
+
+	std::vector<TokensDaLinha> listaDeTokensDoArquivo;
+
+	void adicionarTokenDaLinha(TokensDaLinha linha);
+
+	void PreProcessamento(std::string conteudoArquivo); //recebe o conteudo do arquivo em uma string e salva os tokens na listaDeTokensDoArquivo
+
+	std::string RemoveComentarios(std::string arquivoConteudo); //remove tudo que tiver entre ; e \n
+
+	std::string RemoveTabulacoes(std::string fileString); //substitui tabulações por espaços em branco
+
+	std::string RemoveEspacosEmBrancoExtras(std::string arquivoConteudo); //remove vários casos de espaços em branco como duplos espaços ou espaços antes de \n
+
+	std::string JuntaLabelEOperacao(std::string arquivo); //quando se tem uma label sozinha em uma linha, coloca a linha com tokens abaixo dela na mesma linha da linha da label
 
 	Montador();
 	~Montador();
