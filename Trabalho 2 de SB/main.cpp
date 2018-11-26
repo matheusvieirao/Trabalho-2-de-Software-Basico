5d46673bde33a4f5c576a2348c5d8d1aee315c5b
nikson <nik.son18@hotmail.com>
2018-11-26 15:08:31 -0200

    adicionado verificacao de extensao no final e arquivo de saida com mesmo nome do de entrada

--- Trabalho 2 de SB/main.cpp
+++ Trabalho 2 de SB/main.cpp
@@ -9,12 +9,28 @@ A execução do programa começa e termina ali.
 #include "Tradutor.h"
 
 int main(int argc, char* argv[]) {
+	if(argc<2){//verifica se algum arquivo de entrada foi especificado
+		std::cout << "Erro: Arquivo .asm não identificado"<< std::endl;
+		return -1;
+	}
 	std::string nomeArquivo = argv[1]; //nome do arquivo de entrada .asm
 	std::ifstream fileAsm(nomeArquivo); //ptr pro arquivo de entrada .asm
 	std::ofstream fileS ("arquivo.s"); //ptr pro arquivo de saida .s
 	std::stringstream  conteudoArquivoAsm; //conteudo arquivo .asm no assembly inventado
 	std::string conteudoArquivoIA32; //conteudo arquivo .s traduzido pra IA32
 	Tradutor tradutor; //classe que contem as funções que fazem a tradução do assembly inventado pro IA-32
+
+	int tam_nome = nomeArquivo.length();
+	size_t pos = nomeArquivo.find(".asm"); //procura .asm no nome
+
+	if(pos!=tam_nome-4){ //verifica se o nome foi digitado com .asm, se nao adiciona antes de tentar abrir
+		fileAsm.open(nomeArquivo + ".asm");
+		fileS.open(nomeArquivo + ".s"); 
+	}else{
+		fileAsm.open(nomeArquivo + ".asm");
+		fileS.open(nomeArquivo.substr(0,pos) + ".s");
+	}
+
 
 	if (fileAsm.is_open() && fileAsm.good()) {
 		conteudoArquivoAsm << fileAsm.rdbuf();
